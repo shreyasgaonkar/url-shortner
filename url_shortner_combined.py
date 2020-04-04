f63d8ca9a22060e540112efaa12ee41c2cb4dff9
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2020-04-04 16:43:52 +0100

    Add URL check rule
    - Check for 301/307 multiple redirects and prevent them from using the
    service
    - Update html to handle 403 error for the above use-case
    - Update README

--- url_shortner_combined.py
+++ url_shortner_combined.py
@@ -1,6 +1,7 @@
 import json
 import hashlib
 import boto3
+import requests
 
 DDB = boto3.client('dynamodb', region_name='us-west-2')
 TABLENAME = 'url-shortner-new'
@@ -31,6 +32,19 @@ def create_url(event):
     # HTTP or HTTPs, send it to HTTP and let server redirect to HTTPS
 
     long_url = f"http://{new_str}"
+
+    # Check if webpage is legit
+    url_validity = check_valid_url(long_url)
+    print(url_validity)
+    if url_validity is not True:
+        return {
+            "statusCode": 403,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps("Enter a different URL. Redirects aren't allowed")
+        }
+
     short_url = hashlib.md5(long_url.encode('utf-8')).hexdigest()[:6]
 
     try:
@@ -97,3 +111,25 @@ def get_url(event):
 
     except Exception as exception:
         print(f"Unable to query. Error: {exception}")
+
+
+def check_valid_url(url):
+    try:
+        r = requests.head(url, timeout=1, allow_redirects=True)
+        r.raise_for_status()
+        status_code = r.status_code
+        print(status_code)
+        if status_code in (301, 307):
+            raise requests.exceptions.HTTPError("Enter a different URL. Redirects aren't allowed")
+
+    except requests.exceptions.Timeout as exception:
+        return f"Timeout Error: {exception}"
+    except requests.exceptions.HTTPError as exception:
+        print("Raised error")
+        print(f"HTTP Error: {exception}")
+        return exception
+    except requests.exceptions.ConnectionError as exception:
+        return f"Error Connecting: {exception}"
+    except requests.exceptions.RequestException as exception:
+        return f"Something went wrong. Error: {exception}"
+    return r.status_code == requests.codes.ok
